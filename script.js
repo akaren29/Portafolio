05b57086b3e38a005a3d23361ed38d5f7735210c
akaren29 <akjassoj@outlook.com>
2022-03-23 18:00:29 -0600

    bug en menu responsive

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let navMenu = document.querySelector('.ul-inicio');
 let navToggle = document.querySelector('.nav-toggle')
+let enlaces = document.querySelectorAll('.enlaces')
 
 navToggle.addEventListener('click', ()=> {
     navMenu.classList.toggle('nav-menu_visible')
 })
+
+
+enlaces.forEach(element => 
+    element.addEventListener('click', ()=>{
+        navMenu.classList.remove('nav-menu_visible')
+    })
+)
